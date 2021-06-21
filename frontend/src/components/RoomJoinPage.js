3c442aade0452c3a29bfc0531a338d685de45b3c
Sujoy Ghosh <ghoshsujoy336@gmail.com>
2021-06-21 17:50:04 +0530

    room api, component added

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -5,101 +5,63 @@ import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 function RoomJoinPage({ history }) {
-  const [guest_can_pause, setGCP] = useState(true);
-  const [votes_to_skip, setVTS] = useState(2);
+  const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
 
-  const handleVotes = (e) => {
-    //setVTS(parseInt(e.target.value));
-    setVTS(e.target.value);
-  };
+  const joinRoom = () => {
+    console.log("joining room");
+    console.log(roomCode);
 
-  const handlRadioChange = (e) => {
-    setGCP(e.target.value === "true" ? true : false);
-  };
-
-  const createRoom = () => {
-    console.log("creating room");
-    console.log(guest_can_pause, votes_to_skip);
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        guest_can_pause,
-        votes_to_skip,
+        code: roomCode,
       }),
     };
-    fetch("/api/create-room", requestOptions)
-      .then((res) => res.json())
-      .then((data) => history.push("/room/" + data.code));
+    fetch("/api/join-room", requestOptions)
+      .then((res) => {
+        if (res.ok) {
+          history.push(`/room/${roomCode}`);
+          setRoomCode("");
+        } else {
+          setError("Room not found");
+        }
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
-    <Grid container spacing={1}>
+    <Grid container spacing={1} className={"page"}>
       <Grid item xs={12} align="center">
-        <Typography component="h4" variant="h4" gutterBottom>
-          Create A Room
+        <Typography variant="h4" component="h4">
+          Join a Room
         </Typography>
       </Grid>
-      <Grid
-        item
-        container
-        direction="column"
-        justify="center"
-        xs={12}
-        alignItems="center"
-      >
-        <FormHelperText>
-          <div align="center">Guest Control of Playback State</div>
-        </FormHelperText>
-        <RadioGroup row defaultValue="true" onChange={handlRadioChange}>
-          <FormControlLabel
-            value="true"
-            control={<Radio color="secondary" />}
-            label="Play/Pause"
-            labelPlacement="bottom"
-          />
-          <FormControlLabel
-            value="false"
-            control={<Radio color="secondary" />}
-            label="No Control"
-            labelPlacement="bottom"
-          />
-        </RadioGroup>
-      </Grid>
       <Grid item xs={12} align="center">
-        <FormControl>
-          <TextField
-            required={true}
-            type="number"
-            defaultValue={1}
-            onChange={handleVotes}
-            inputProps={{
-              min: 1,
-              style: { textAlign: "center" },
-            }}
-          />
-          <FormHelperText>
-            <div align="center">Votes Required To Skip Song</div>
-          </FormHelperText>
-        </FormControl>
+        <TextField
+          error={error}
+          color="secondary"
+          label="Code"
+          onChange={(e) => setRoomCode(e.target.value)}
+          value={roomCode}
+          helperText={error}
+          placeholder="Enter a Room Code"
+          variant="outlined"
+        />
       </Grid>
-      <Grid container justify="space-around">
-        <Button color="secondary" to="/" component={Link}>
-          back
+      <Grid container justify="space-around" style={{ height: 50 }}>
+        <Button color="secondary" component={Link} to="/">
+          Back
         </Button>
         <Button
-          color="secondary"
           variant="contained"
-          onClick={() => createRoom()}
+          color="secondary"
+          onClick={() => joinRoom()}
         >
-          Create A Room
+          Enter Room
         </Button>
       </Grid>
     </Grid>
